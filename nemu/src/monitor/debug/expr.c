7a886553330441e963a9b392f0ca9761cee0a00b
tracer-ics2017 <tracer@njuics.org>
2024-03-06 23:55:10 -0800

    >  compile 2113301 xyang Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  23:55:10 up 10:46,  1 user,  load average: 0.46, 0.27, 0.20 d870d4d1c111858a9ed08a7983e568667d0a2f4

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -216,7 +216,8 @@ uint32_t eval(int p , int q) // tokens[p] and tokens[q] NOT CHARACTER[p] AND CHA
     switch (tokens[op].type)
     {
     case '+':
-      return (val1 + val2);
+      printf("%d\n",val1+val2);
+      return val1 + val2;
       break;
     case '-':
       return val1 - val2;
