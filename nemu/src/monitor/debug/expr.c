b4fbd3b71e804bceca1f858d2d4b9ec91eb5f2d1
tracer-ics2017 <tracer@njuics.org>
2024-03-06 23:52:18 -0800

    >  compile 2113301 xyang Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  23:52:18 up 10:43,  1 user,  load average: 0.15, 0.15, 0.16 500f7a7d3b30cbf28e6dfea0c945103589c5a9bf

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -212,6 +212,7 @@ uint32_t eval(int p , int q) // tokens[p] and tokens[q] NOT CHARACTER[p] AND CHA
     int op = dominant_op(p,q);
     int val1 = eval(p,op-1);
     int val2 = eval(op+1,q);
+    printf("%d %d\n",val1,val2);
     switch (tokens[op].type)
     {
     case '+':
