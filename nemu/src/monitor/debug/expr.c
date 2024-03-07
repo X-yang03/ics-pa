926dd0e679e20c154c79d30c3295d7a0b310413b
tracer-ics2017 <tracer@njuics.org>
2024-03-06 23:11:42 -0800

    >  compile 2113301 xyang Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  23:11:42 up 10:03,  1 user,  load average: 0.33, 0.39, 0.24 1cc9dd868df707446b9feb422b6dbc8b5e936914

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,9 +5,10 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include<stdlib.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ, TK_DEC,TK_REG
+  TK_NOTYPE = 256, TK_EQ,TK_NEQ, TK_DEC,TK_HEX,TK_REG
 
   /* TODO: Add more token types */
 
@@ -25,7 +26,9 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},         // equal
-  {"([1-9][0-9]*|0)",TK_DEC}, //decimial
+  {"!=",TK_NEQ},
+  {"[1-9][0-9]*|0",TK_DEC}, //decimial
+  {"0[xX][0-9a-fA-F]+",TK_HEX},
   {"\\-",'-'},
   {"\\*",'*'},
   {"\\/",'/'},
@@ -61,10 +64,11 @@ void init_regex() {
 
 typedef struct token {
   int type;
+  int priority; // used to find dominant op
   char str[32];
 } Token;
 
-Token tokens[32];
+Token tokens[32]; // at most 32 tokens in a sentence
 int nr_token;
 
 static bool make_token(char *e) {
@@ -89,11 +93,28 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
+        tokens[nr_token].type = rules[i].token_type;
+        tokens[nr_token].priority = -1;
 
         switch (rules[i].token_type) {
-          default: TODO();
+          case '+': case '-':  
+            tokens[nr_token].priority = 1;
+            break;
+
+          case '*': case '/':
+              tokens[nr_token].priority = 2;
+            break;
+
+          case TK_DEC: case TK_HEX: case TK_REG:
+            Assert(substr_len<32,"Length of numbers should be no more than 31!\n");
+            //KISS protocol
+            strncpy(substr_start,tokens[nr_token].str,substr_len);  //copy the string
+            break;
+
+          default: break;
         }
 
+        nr_token++;
         break;
       }
     }
@@ -107,8 +128,107 @@ static bool make_token(char *e) {
   return true;
 }
 
-uint32_t eval(int p , int q)
+bool check_parentheses(int p,int q){
+  int par = 0;
+  if(tokens[p].type == '(' && tokens[q].type == ')'){
+      for(int i = p + 1;i < q;i++){
+        if(tokens[i].type == '(') par++;
+        else if(tokens[i].type == ')') par--;
+    }
+    if(par == 0) return true;
+  }
+  return false;
+}
+
+bool check_priority(int p, int q){
+    if(tokens[p].priority != -1 && tokens[q].priority != -1){
+      return tokens[p].priority - tokens[q].priority;
+    }
+    return false;
+}
+
+int dominant_op(int p , int q){ // find dominant operator between p and q
+  int in_parentheses = 0;
+  int min_priority = 10;
+  int _dominant = 0;
+  for(int i = p; i <= q; i++){
+    if(tokens[i].type == '(') in_parentheses++;
+    else if (tokens[i].type == ')') in_parentheses--;
+
+    if(in_parentheses == 0 && tokens[i].priority != -1){ // with no parentheses
+        if(tokens[i].priority < min_priority){
+          min_priority = tokens[i].priority;
+          _dominant = i;
+        }
+        else if(tokens[i].priority == min_priority){
+          _dominant = i;
+        }
+
+    }
+  }
+  return _dominant;
+
+}
+
+uint32_t eval(int p , int q) // tokens[p] and tokens[q] NOT CHARACTER[p] AND CHARACTER[q]!
 {
+  Assert(p<=q,"Bad Expression!");
+  if(p == q){ // a single token
+    int val = 0;
+    switch (tokens[p].type)
+    {
+    case TK_DEC:
+      return atoi(tokens[p].str);
+      break;
+    
+    case TK_HEX:
+      
+      sscanf(tokens[p].str,"%x",&val);
+      return val;
+
+    case TK_REG:
+      if (strcmp(tokens[p].str , "$eax") == 0) return cpu.eax;
+      else if (strcmp(tokens[p].str , "$eax") == 0) return cpu.eax;
+      else if (strcmp(tokens[p].str , "$ebx") == 0) return cpu.ebx;
+      else if (strcmp(tokens[p].str , "$ecx") == 0) return cpu.ecx;
+      else if (strcmp(tokens[p].str , "$edx") == 0) return cpu.edx;
+      else if (strcmp(tokens[p].str , "$ebp") == 0) return cpu.ebp;
+      else if (strcmp(tokens[p].str , "$esp") == 0) return cpu.esp;
+      else if (strcmp(tokens[p].str , "$esi") == 0) return cpu.esi;
+      else if (strcmp(tokens[p].str , "$edi") == 0) return cpu.edi;
+      else if (strcmp(tokens[p].str , "$eip") == 0) return cpu.eip;
+    default:
+      break;
+    }
+
+  }
+  else if(check_parentheses(p,q)){
+    eval(p+1,q-1);
+  }
+  else{
+    int op = dominant_op(p,q);
+    int val1 = eval(p,op-1);
+    int val2 = eval(op+1,q);
+    switch (tokens[op].type)
+    {
+    case '+':
+      return val1 + val2;
+      break;
+    case '-':
+      return val1 - val2;
+      break;
+    case '*':
+      return val1 * val2;
+      break;
+    case '/':
+      return val1 / val2;
+      break;
+    default:
+      Assert(0,"Bad operation!\n");
+      break;
+    }
+  }
+
   return 0;
 }
 
@@ -119,8 +239,7 @@ uint32_t expr(char *e, bool *success) {
     return 0;
   }
 
+  return eval(0,nr_token-1);
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
 
-  return 0;
 }
