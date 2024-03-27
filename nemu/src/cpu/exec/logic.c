0c37e561a86f7382b292d1a8245e32d0f8880323
tracer-ics2017 <tracer@njuics.org>
2024-03-27 05:07:51 -0700

    >  compile 2113301 xyang Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  05:07:51 up 12:23,  1 user,  load average: 0.06, 0.23, 0.24 1b5f41dd9d7d40137028a094a2581bb537b0490c

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -3,10 +3,10 @@
 make_EHelper(test) {
   //TODO();
   rtl_and(&t0, &id_dest->val, &id_src->val);
-  rtl_li(&t1, 0);
-  rtl_set_CF(&t1);
-  rtl_set_OF(&t1);
   rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
+  
 
   print_asm_template2(test);
 }
@@ -46,21 +46,31 @@ make_EHelper(or) {
 }
 
 make_EHelper(sar) {
-  TODO();
+  //TODO();
+  rtl_sar(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+  rtl_update_ZFSF(&id_dest->val,id_dest->width);
+
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(sar);
 }
 
 make_EHelper(shl) {
-  TODO();
-  // unnecessary to update CF and OF in NEMU
+  //TODO();
+  rtl_shl(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+  rtl_update_ZFSF(&id_dest->val,id_dest->width);
 
   print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
-  TODO();
+  //TODO();
+  rtl_shr(&t0,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t0);
+  rtl_update_ZFSF(&id_dest->val,id_dest->width);
+
   // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(shr);
@@ -75,7 +85,10 @@ make_EHelper(setcc) {
 }
 
 make_EHelper(not) {
-  TODO();
+  rtl_mv(&t1,&id_dest->val);
+  rtl_not(&t1);
+  operand_write(id_dest,&t1);
+
 
   print_asm_template1(not);
 }
