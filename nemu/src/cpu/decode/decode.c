b8efc2a90e9010b1e405587b37eb86e643d14b19
tracer-ics2017 <tracer@njuics.org>
2024-03-27 06:03:28 -0700

    >  compile 2113301 xyang Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  06:03:28 up 12:45,  1 user,  load average: 0.41, 0.33, 0.26 c380050afdb60e7c924e2f19f3258a0b904fada

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -40,7 +40,7 @@ static inline make_DopHelper(SI) {
    */
   //TODO();
   op->simm = instr_fetch(eip, op->width);
-  rtl_sext(&t0,&t0,op->width);
+  //rtl_sext(&t0,&t0,op->width);
   op->simm = t0;
   rtl_li(&op->val, op->simm);
 
