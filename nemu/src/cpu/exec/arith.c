b5fd7c959f157794d7080bc321294cb33ef6e7df
tracer-ics2017 <tracer@njuics.org>
2024-03-30 05:58:49 -0700

    >  compile 2113301 xyang Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  05:58:49 up 17:59,  1 user,  load average: 0.34, 0.08, 0.03 7a96a807fa64a2a7a63e0464219e4466d59b5806

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -94,15 +94,18 @@ make_EHelper(dec) {
 }
 
 make_EHelper(neg) {
-  //TODO();
-  if(!id_dest->val)rtl_set_CF(&tzero);
-  else{rtl_addi(&t0,&tzero,1);rtl_set_CF(&t0);}
+  if (id_dest->val == 0) {
+    rtl_set_CF(&tzero);
+  } else {
+    rtl_addi(&t0, &tzero, 1);
+    rtl_set_CF(&t0);
+  }
 
-  rtl_add(&t0,&tzero,&id_dest->val);
-  t0 = -1 * t0;
-  operand_write(id_dest,&t0);
+  rtl_add(&t0, &tzero, &id_dest->val);
+  t0 = -t0;
+  operand_write(id_dest, &t0);
 
-  rtl_update_ZFSF(&t0, id_dest->width); 
+  rtl_update_ZFSF(&t0, id_dest->width);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
   rtl_xor(&t1, &id_dest->val, &t2);
