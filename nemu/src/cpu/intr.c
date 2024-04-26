1b74d4ce170af44f85aca0aa7fdc484f1a8ba91d
2113301-Zhu Xia Yang <1979954151@qq.com>
2024-04-26 01:13:29 -0700

    still implementing int

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,14 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+  //TODO();
+  rtl_push(&cpu.eflags.val);
+  rtl_push(&cpu.cs);
+  rtl_push(&ret_addr);
+  rtl_li(&t0, vaddr_read(cpu.idtr.base + NO * 8, 2));
+  rtl_li(&t1, vaddr_read(cpu.idtr.base + NO * 8 + 6, 2));
+  decoding.jmp_eip = (t1 << 16)| (0xffff & t0);
+  decoding.is_jmp = 1;
 }
 
 void dev_raise_intr() {
