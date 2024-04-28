63fc195781a74b202eb0bbace794f1028bb89215
tracer-ics2017 <tracer@njuics.org>
2024-04-28 00:02:29 -0700

    >  compile 2113301 xyang Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  00:02:29 up  7:14,  1 user,  load average: 0.08, 0.21, 0.38 ff9e6d0a5389e71a2b1b37bd8ed82513f9bb4258

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -3,7 +3,9 @@
 make_EHelper(test) {
   //TODO();
   rtl_and(&t0,&id_dest->val,&id_src->val);
-  printf("=====\nres:%d\n====\n",t0);
+  printf("=====\n");
+  printf("and %08x, %08x\n",id_dest->val,id_src->val);
+  printf("res:%d\n=====\n",t0);
   rtl_update_ZFSF(&t0,id_dest->width);
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
