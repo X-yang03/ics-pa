1fdcedd99674e435c27cabfedd29c2dbc1ff773c
tracer-ics2017 <tracer@njuics.org>
2024-05-09 02:11:47 -0700

    >  compile 2113301 xyang Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  02:11:47 up  7:57,  1 user,  load average: 0.19, 0.32, 0.24 8867fcb5d379250a71d737e8fea142a37d5dcc13

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -198,15 +198,17 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
   //TODO();
-  cpu.eflags.ZF = (((~0 >> (32 - width * 8)) & *result) == 0);
+  rtl_andi(&t0,result,(0xffffffffu>>(4-width)*8));
+  rtl_eq0(&t0,&t0);
+  rtl_set_ZF(&t0);
   
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
   //TODO();
-  rtl_msb(&t0, result, width);
-  cpu.eflags.SF = t0;
+  rtl_msb(&t0,result,width);
+  rtl_set_SF(&t0);
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
