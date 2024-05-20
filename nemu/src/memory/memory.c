d5acd630942c7c2f4769ccc8851728344f9398a9
tracer-ics2017 <tracer@njuics.org>
2024-05-20 08:25:27 -0700

    >  compile 2113301 xyang Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  08:25:27 up  5:23,  1 user,  load average: 0.53, 0.26, 0.21 e06a8b325d362b1ba6c32041fddb221d1d216257

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -28,7 +28,7 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 paddr_t page_translate(vaddr_t addr, int operation){
   CR0 cr0 = (CR0)cpu.cr0;
-  if(cr0.paging){
+  if(cr0.protect_enable && cr0.paging){
     //Log("translate\n");
     CR3 cr3 = (CR3)cpu.cr3;
     PDE* pgdir=(PDE*)PTE_ADDR(cr3.val);
