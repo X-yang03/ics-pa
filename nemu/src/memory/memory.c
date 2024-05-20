c7fb57c7daed5eb9798cf29f22869a4a9c7227af
tracer-ics2017 <tracer@njuics.org>
2024-05-20 06:49:26 -0700

    >  compile 2113301 xyang Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  06:49:26 up  3:47,  1 user,  load average: 0.28, 0.37, 0.30 f8af65eb8e308942dee2f27de957e978aaf454de

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -73,7 +73,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
     uint32_t high_page = paddr_read(paddr2, len2);
     Log("read %d bytes %08x\n", len2, high_page);
     
-    uint32_t page = (high_page)<<len2*8 | low_page;
+    uint32_t page = (high_page)<<len1*8 | low_page;
     Log("data: %08x\n",page);
     Assert(0,"here\n");
     return page; 
