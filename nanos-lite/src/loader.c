9ba6f1650ea5fe86222ee4b64a197ab4cdb9579c
tracer-ics2017 <tracer@njuics.org>
2024-05-20 07:57:40 -0700

    >  run 2113301 xyang Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  07:57:40 up  4:55,  1 user,  load average: 0.63, 0.39, 0.23 c2f87704db717c6b6eb318f3de3a87e757ddeee5

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,7 +15,6 @@ uintptr_t loader(_Protect *as, const char *filename) {
   // ramdisk_read(DEFAULT_ENTRY,0,len);
   int fd = fs_open(filename, 0, 0);
   size_t size = fs_filesz(fd);
-  Log("Load [%d] %s with size: %d page: %d", fd, filename, size, size/PGSIZE+1);
   void* pa = NULL;
   void* va = (void*)DEFAULT_ENTRY; //beginning of the data
   int remain_size = size;
@@ -27,6 +26,7 @@ uintptr_t loader(_Protect *as, const char *filename) {
     fs_read(fd, pa, PGSIZE);
   }
   //fs_read(fd, (void*)DEFAULT_ENTRY, size);
+  Log("Load [%d] %s with size: %d page: %d", fd, filename, size, size/PGSIZE+1);
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 
