5082b0ffd7a7109d92e6c3bda1b20c75e96fdf4c
tracer-ics2017 <tracer@njuics.org>
2024-05-20 06:43:39 -0700

    >  compile 2113301 xyang Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  06:43:39 up  3:41,  1 user,  load average: 0.27, 0.32, 0.26 8a15edfabf5fdd258ba53e4718c8ffe2e418afb9

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -63,7 +63,7 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
   if(PTE_ADDR(addr) != PTE_ADDR(addr+len-1)){
-    Assert(0,"read across different pages!! %08x to %08x\n", addr, addr+len);
+    Assert(0,"read across different pages!! %08x to %08x\n", addr, addr+len-1);
     int len1 = 0x1000 - OFF(addr);
     paddr_t paddr1 = page_translate(addr, vread);
     uint32_t low_page = paddr_read(paddr1, len1);
@@ -85,7 +85,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
   if(PTE_ADDR(addr) != PTE_ADDR(addr+len-1)){
-    Assert(0,"write across different pages!! %08x to %08x\n", addr, addr+len);
+    Assert(0,"write across different pages!! %08x to %08x\n", addr, addr+len-1);
     int len1 = 0x1000 - OFF(addr);
     paddr_t paddr1 = page_translate(addr, vwrite);
     uint32_t low_data = data & (~0u>>(32-len1*8));
