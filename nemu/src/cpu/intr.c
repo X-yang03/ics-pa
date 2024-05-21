0127a0502103ae477d5dab6d9502af83c003a235
tracer-ics2017 <tracer@njuics.org>
2024-05-21 08:24:02 -0700

    >  compile 2113301 xyang Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  08:24:02 up 19 min,  1 user,  load average: 0.16, 0.24, 0.19 634ee4186735efaaa5065d18269df97271d21c93

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -15,6 +15,9 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_li(&t1, vaddr_read(cpu.idtr.base + NO * 8 + 6, 2));
   decoding.jmp_eip = (t1 << 16)| (0xffff & t0);
   decoding.is_jmp = 1;
+  if(NO == 32){
+    Log("time int\n");
+  }
 }
 
 void dev_raise_intr() {
